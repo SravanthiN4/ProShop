5ea40c76fb85d39c59d71803e05ae620a89db6cd
Sravanthi <snedunoori44@gmail.com>
2024-06-28 17:42:28 -0400

    frontend navbar products and product page implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Container} from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <h1>Welcome to ProShop!</h1>
+    <>
+      <Header />
+      <main class="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+        
+      </main>
+      <Footer/>
+    </>
+    
   )
 }
 
