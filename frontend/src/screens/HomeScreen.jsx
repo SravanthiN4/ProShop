ea51917027c11c9057382f9764a7e38fe25821d6
Sravanthi <snedunoori44@gmail.com>
2024-06-28 20:16:58 -0400

    middleware configured to concurrently run front and backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,15 +1,29 @@
 import {Row, Col} from 'react-bootstrap';
-import products from '../products';
 import React from 'react';
-import Products from '../components/Products';
+import { useEffect, useState } from 'react';
+import Product from '../components/Products';
+import axios from 'axios';
 const HomeScreen = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const { data } = await axios.get('/api/products');
+                setProducts(data);
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        }
+        fetchProducts();
+    }, []);
   return (
     <>
       <h1> Latest Products </h1>
       <Row>
-        {products.map((product) => (
+        {
+        products.map((product) => (   
             <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
-                <Products product={product} />
+                <Product product={product} />
             </Col>
         ))}
       </Row>
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import products from '../products';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import {Row, Col, ListGroup, Image, Card, Button, ListGroupItem } from 'react-bootstrap';
 import Rating from '../components/Rating';
 
 const ProductScreen = () => {
+    const [product, setProduct] = useState({});
     const { id: productId } = useParams();
-    const product = products.find((p) => p._id === productId)
+    
+    useEffect(() => {
+        const fetchProduct = async() => {
+            const { data } = await axios.get(`/api/products/${productId}`);
+            setProduct(data);
+        }
+        fetchProduct();
+    }, [productId]);
+
   return (
     <>
       <Link className="btn btn-light my-3" to="/">
