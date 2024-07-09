a824db224a1615da04f448877cbc9e0bbd7b6ee0
Sravanthi <snedunoori44@gmail.com>
2024-07-08 23:17:15 -0400

    User authorization and authentication implementation as well as login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import {Container} from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
-
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
   return (
     <>
@@ -15,6 +16,7 @@ const App = () => {
         
       </main>
       <Footer/>
+      <ToastContainer/>
     </>
     
   )
